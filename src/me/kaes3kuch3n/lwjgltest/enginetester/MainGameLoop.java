43cffe21acf2fe75e5ddcd934900da45fd9c0f8c
Luis Hankel <kaes3kuch3n@gmail.com>
2017-06-29 18:46:46 +0200

    Added OBJ file loader

--- src/me/kaes3kuch3n/lwjgltest/enginetester/MainGameLoop.java
+++ src/me/kaes3kuch3n/lwjgltest/enginetester/MainGameLoop.java
@@ -9,6 +9,7 @@ import me.kaes3kuch3n.lwjgltest.models.RawModel;
 import me.kaes3kuch3n.lwjgltest.models.TexturedModel;
 import me.kaes3kuch3n.lwjgltest.renderengine.DisplayManager;
 import me.kaes3kuch3n.lwjgltest.renderengine.Loader;
+import me.kaes3kuch3n.lwjgltest.renderengine.OBJLoader;
 import me.kaes3kuch3n.lwjgltest.renderengine.Renderer;
 import me.kaes3kuch3n.lwjgltest.shaders.StaticShader;
 import me.kaes3kuch3n.lwjgltest.textures.ModelTexture;
@@ -21,115 +22,17 @@ public class MainGameLoop {
 		Loader loader = new Loader();
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
-		
-		/*float[] vertices = {
-				-0.5f, 0.5f, 0, 	//V0
-				-0.5f, -0.5f, 0, 	//V1
-				0.5f, -0.5f, 0, 	//V2
-				0.5f, 0.5f, 0 		//V3
-		};
-		
-		int[] indices = {
-				0, 1, 3, 	//Top left triangle (V0, V1, V3)
-				3, 1, 2		//Bottom right triangle (V3, V1, V2)
-		};
-		
-		float[] textureCoords = {
-				0, 0, 	//V0
-				0, 1, 	//V1
-				1, 1, 	//V2
-				1, 0	//V3
-		};*/
-		
-		float[] vertices = {			
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,0.5f,-0.5f,		
 				
-				-0.5f,0.5f,0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				0.5f,0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				-0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f
-				
-		};
-		
-		float[] textureCoords = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-				
-		};
-		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
-		
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		RawModel model = OBJLoader.loadObjModel("stall", loader);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
 		TexturedModel staticModel = new TexturedModel(model, texture);
 		
-		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		Entity entity = new Entity(staticModel, new Vector3f(0, -3, -20), 0, 120, 0, 1);
 		
 		Camera camera = new Camera();
 		
 		while(!Display.isCloseRequested()) {
-			entity.increaseRotation(1, 1, 0);
+			entity.increaseRotation(0, 0, 0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
